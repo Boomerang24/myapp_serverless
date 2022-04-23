90614b89634987bb569dcd269bcc0262e0e62f25
Boomerang24 <alexcomoed@gmail.com>
2022-04-22 20:31:10 -0500

    changed fetch to prevent CORS temporarily - To Fix

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,16 +6,16 @@ export const useFetch = () => {
   useEffect(() => {
     const doFetch = async () => {
       try {
-        // const res = await fetch("https://randomuser.me/api/?results=20");
-        const res = await fetch(
-          "https://tjsjiib216.execute-api.us-east-1.amazonaws.com/default/getAPIUsers",
-          {
-            mode: "cors",
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-            },
-          }
-        );
+        const res = await fetch("https://randomuser.me/api/?results=20");
+        // const res = await fetch(
+        //   "https://tjsjiib216.execute-api.us-east-1.amazonaws.com/default/getAPIUsers",
+        //   {
+        //     mode: "cors",
+        //     headers: {
+        //       "Access-Control-Allow-Origin": "*",
+        //     },
+        //   }
+        // );
         const json = await res.json();
         const users = json.results;
         setResponse(users);
