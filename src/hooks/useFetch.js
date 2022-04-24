5ef9a54d37270e4430a8f425458871ba84acd3b1
Boomerang24 <alexcomoed@gmail.com>
2022-04-24 18:42:46 -0500

    Fixed CORS issue with backend

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,18 +6,12 @@ export const useFetch = () => {
   useEffect(() => {
     const doFetch = async () => {
       try {
-        const res = await fetch("https://randomuser.me/api/?results=20");
-        // const res = await fetch(
-        //   "https://tjsjiib216.execute-api.us-east-1.amazonaws.com/default/getAPIUsers",
-        //   {
-        //     mode: "cors",
-        //     headers: {
-        //       "Access-Control-Allow-Origin": "*",
-        //     },
-        //   }
-        // );
+        // const res = await fetch("https://randomuser.me/api/?results=20");
+        const res = await fetch(
+          "https://tjsjiib216.execute-api.us-east-1.amazonaws.com/dev/my-users"
+        );
         const json = await res.json();
-        const users = json.results;
+        const users = await json.users;
         setResponse(users);
       } catch (error) {
         console.log(error);
